2332524ebe8c328389fe07e3d880262a7f91f46f
Chumani <chumani@vetro.co.za>
2021-03-23 13:48:12 +0200

    finished auth

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+const { model } = require("mongoose");
+
+if(process.env.NODE_ENV === 'production'){
+// we are in production - return the prod  se keys 
+
+module.exports = require('./prod');
+
+} else {
+ // we are in development - return  dev keys 
+
+ module.exports = require('./dev');
+
+
+}
